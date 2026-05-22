c0bb71abc66ffdd837294d5ff0fbc4e04db0f60a
steven <steven841221@gmail.com>
2026-05-23 03:37:01 +1000

    feat: transformation step setup

--- airflow-dbt-analytics-platform/airflow/dags/orchestrator.py
+++ airflow-dbt-analytics-platform/airflow/dags/orchestrator.py
@@ -2,6 +2,7 @@ import sys
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
 # 因為 Airflow 執行 DAG 時，預設通常只有 /opt/airflow/dags 所以 from insert_records import main
 # Python 只會找 /opt/airflow/dags/insert_records.py 但檔案其實在 /opt/airflow/api-request/insert_records.py
 # 所以必須把這個路徑加進 Python module search path /opt/airflow/api-request/insert_records.py
@@ -31,12 +32,7 @@ default_args = {
 # Python task functions
 # =========================================================
 
-def extract():
-    print("Running extract step...")
 
-
-def transform():
-    print("Running transform step...")
 
 
 def load():
@@ -72,21 +68,24 @@ with DAG(
     # =====================================================
     # Task 2
     # =====================================================
-    transform_task = PythonOperator(
-        task_id="transform_data",
-        python_callable=transform,
+
+    transform_task = BashOperator(
+      task_id="transform_data",
+      # 直接切換到 Airflow 容器內的 dbt 專案路徑，並執行 dbt run
+      # 明確告訴它 profiles.yml 檔案在 /opt/airflow/dbt 底下
+      bash_command="cd /opt/airflow/dbt/de_project && dbt run --profiles-dir /opt/airflow/dbt",
     )
 
     # =====================================================
     # Task 3
     # =====================================================
-    load_task = PythonOperator(
-        task_id="load_data",
-        python_callable=load,
-    )
+    # load_task = PythonOperator(
+    #     task_id="load_data",
+    #     python_callable=load,
+    # )
 
     # =====================================================
     # Task dependencies
     # =====================================================
 
-    extract_task >> transform_task >> load_task
+    extract_task >> transform_task
